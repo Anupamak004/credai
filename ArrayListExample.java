b6834d31051673e662db62ff479d6bb299213698
Anupama <anumsman@gmail.com>
2026-01-19 22:41:43 +0530

    LinkedList

--- ArrayListExample.java
+++ ArrayListExample.java
@@ -1,12 +1,48 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class ArrayListExample {
     public static void main(String[] args) {
-        ArrayList<String> cars = new ArrayList<String>();
-        cars.add("Volvo");
-        cars.add("BMW");
-        cars.add("Ford");
-        cars.add("Mazda");
-        System.out.println(cars);
+        ArrayList<Integer> numArray = new ArrayList<>();
+        numArray.add(1);
+        numArray.add(5);
+        numArray.add(7);
+        numArray.set(0, 5);
+        numArray.add(1, 9);
+        numArray.remove(5);
+        System.out.println(numArray.indexOf(8));
+        numArray.remove(Integer.valueOf(5));
+
+        if (numArray.contains(5)) {
+            System.out.println("found");
+        } else {
+            System.out.println("not found");
+        }
+
+        for (int i = 0; i < numArray.size(); i++) {
+            System.out.println(numArray.get(i));
+        }
+
+        numArray.clear();
+        if (numArray.isEmpty()) {
+            System.out.println("Array is empty");
+        }
+        System.out.println(numArray);
     }
 }
+
+// arrayList.add(value) will append value at the end
+// arrayList.set(index, value) will replace the value
+// arrayList.add(index, value) will shift elements to right and insert value at
+// index
+// arrayList.remove(index)
+// for integers use arrayList.remove(Integer.valueOf(value))
+// arrayList.remove(value)
+// arrayList.contains(value) returns boolean checking presence of value
+// arrayList.get(index) returns value at index
+// arrayList.size()
+// arrayList.clear()
+// arrayList.isEmpty()
+// arrayList.toString() returns string representation of arrayList
+// arrayList.indexOf(value) returns index of first occurrence of value else -1
+// arrayList.hasNext() and arrayList.next() for iterators
+// arrayList extends AbstractList and implements List interface
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Create a java file to print "Hello world"
 
 Wrote a java program on arryaList
+
+Wrote a linked list program
